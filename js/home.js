175b15d4a00bf9d457bc4ad6c6bab1038a4f5091
Antonio Fortino <thoniorf@gmail.com>
2016-07-31 14:21:40 +0200

    Updated Index
    Updated Search bar filter disply
    Added Events Placeholder and page section
    Added footer
    Added Back to Top button with slide animation

--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
+/*
+ * Innerbar animations and handlers
+ */
 function innerToggle(target, parent, selector) {
     $(parent + " " + selector).each(function() {
         $(this).hide();
@@ -7,33 +10,49 @@ function innerToggle(target, parent, selector) {
     $("#inner-" + target + "-form input:first").focus();
 }
 var innerbar_timeout;
-var dropdown_timeout;
 $(".navbar-action-group").on('click', "button,a", function() {
     var target = $(this).attr("aria-label");
     if (target !== "cart") {
         innerToggle(target, "#innerbar-popup", "form");
     }
 });
-$("#inner-search-form button[aria-label='filters']").on('click', function() {
-console.log("innerbar stop");
-    clearTimeout(innerbar_timeout);
-    /*  dropdown_timeout = setTimeout(function() {
-          $("#inner-search-form ul[aria-label='filterslist']").dropdown();
-      }, 2500);*/
-});
-$("#inner-search-form ul[aria-label='filterslist']").on('click', "a", function(e) {
-    e.preventDefault();
-    $("#inner-search-form input:first").focus();
-});
 $("#innerbar-popup").on('focus', "input", function() {
-    console.log("innerbar stop");
     clearTimeout(innerbar_timeout);
 });
 $("#innerbar-popup").on('focusout', "form", function() {
-    console.log("innerbar start");
-    if(!$("#inner-search-form ul li a").is(":focus"))
-    innerbar_timeout = setTimeout(function() {
-        $("#innerbar-popup").slideToggle();
-    }, 3000);
+    if (!$("#inner-search-form ul li a").is(":focus"))
+        innerbar_timeout = setTimeout(function() {
+            $("#innerbar-popup").slideToggle();
+        }, 2000);
 
+});
+/*
+ * Back-to-top button animations and handler
+ */
+function backToTopOffset() {
+  var offset = $(document).height() - $("footer").offset().top;
+  console.log(offset);
+    if ($(this).scrollTop() > offset) {
+        $("#back-to-top").fadeIn(duration);
+    } else {
+        $("#back-to-top").fadeOut(duration);
+    }
+
+    if ($("#back-to-top").offset().top + $("#back-to-top").height() >= $("footer").offset().top - 10) {
+        $("#back-to-top").css('position', 'absolute');
+    }
+    if ($(document).scrollTop() + window.innerHeight < $("footer").offset().top)
+        $("#back-to-top").css('position', 'fixed');
+}
+var offset = 300;
+var duration = 300;
+$(window).scroll(function() {
+    backToTopOffset();
+
+});
+$("#back-to-top").on('click', function(e) {
+  e.preventDefault();
+    $("html,body").animate({
+        scrollTop: 0
+    }, duration);
 });
