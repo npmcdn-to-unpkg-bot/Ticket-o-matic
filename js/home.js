0eeb3b0e0faf364588ca99b888634e11a5ecb8b3
Antonio Fortino <thoniorf@gmail.com>
2016-08-11 22:32:37 +0200

    Updated index
    Added other search filters html code
    Added filters radio input event handler
    
    Added new css rules for search by price form group

--- js/home.js
+++ js/home.js
@@ -1,13 +1,14 @@
 /*
  * Innerbar animations and handlers
  */
+ var search_type = 'search';
 function innerToggle(target, parent, selector) {
     $(parent + " " + selector).each(function() {
         $(this).hide();
     });
     $("#inner-" + target + "-form").show();
     $("#innerbar-popup").hide().removeClass("hidden").slideToggle();
-    $("#inner-" + target + "-form input:first").focus();
+    $("#inner-" + target + "-form input:visible:first").focus();
 }
 var innerbar_timeout;
 $(".navbar-action-group").on('click', "button,a", function() {
@@ -25,6 +26,28 @@ $("#innerbar-popup").on('focusout', "form", function() {
             $("#innerbar-popup").slideToggle();
         }, 2000);
 
+});
+function searchPlaceholder(text) {
+  $("#search-group input").filter(":first").attr("placeholder",text);
+  $("#search-group").hide().removeClass("hidden").fadeIn();
+  $("#searchbydate-group").hide();
+  $("#searchbyprice-group").hide();
+}
+$("#inner-search-form").on('click',"input[name=filters]",function(){
+  var type = $(this).val();
+  if(type === 'bydate') {
+    $("#searchbydate-group").hide().removeClass("hidden").fadeIn();
+    $("#searchbyprice-group").hide();
+    $("#search-group").hide();
+  } else if (type === 'byprice') {
+    $("#searchbyprice-group").hide().removeClass("hidden").fadeIn();
+    $("#searchbydate-group").hide();
+    $("#search-group").hide();
+  } else if(type === 'bytitle'){
+    searchPlaceholder("Search by Events Title");
+  } else if(type === 'byloc'){
+    searchPlaceholder("Search by Events Location");
+  }
 });
 /*
  * Back-to-top button animations and handler
