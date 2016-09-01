d4756b021ce29bc7b1be317beec8b0d87c9c8bc8
Antonio Fortino <thoniorf@gmail.com>
2016-09-01 23:06:10 +0200

    Updated geocoding script and db host
    Fixed geocoding bug
    Switched to AWS Postgresql instance
    Dumped localhost db into AWS instance

--- WebContent/js/geocoding.js
+++ WebContent/js/geocoding.js
@@ -1,27 +1,32 @@
 /**
- *  GEOCODING SCRIPT
+ * GEOCODING SCRIPT
  */
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 16,
-          center: {lat: -34.397, lng: 150.644}
-        });
-        var geocoder = new google.maps.Geocoder();
-        geocodeAddress(geocoder, map);     
-      }
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom : 16,
+		center : {
+			lat : -34.397,
+			lng : 150.644
+		}
+	});
+	var geocoder = new google.maps.Geocoder();
+	geocodeAddress(geocoder, map);
+}
 
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = $("[data-name='location']").text().trim();
-        console.log(address);
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
+function geocodeAddress(geocoder, resultsMap) {
+	var address = document.querySelector("[data-name='location']").innerHTML;
+	geocoder.geocode({
+		'address' : address
+	}, function(results, status) {
+		if (status === 'OK') {
+			resultsMap.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+				map : resultsMap,
+				position : results[0].geometry.location
+			});
+		} else {
+			alert('Geocode was not successful for the following reason: '
+					+ status);
+		}
+	});
+}
